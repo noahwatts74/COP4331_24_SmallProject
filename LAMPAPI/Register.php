0d417637faf44fe9ccfa7b8d97e5921b620bdc24
HBobertz <Bobertzx@gmail.com>
2020-06-01 22:11:44 -0400

    working register and unfinished contacts

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -11,12 +11,28 @@ if ($conn->connect_error)
 	returnWithError( $conn->connect_error );
 } 
 else
-{
-
-    $sql = "INSERT INTO Users(Login, Password) VALUES ('" . $inData["Login"] . "', '" . $inData["Password"] . "')";
-    $conn->query($sql);
-    sendResultInfoAsJson($inData["Login"],$inData["Password"]);
-
+{   
+    $sql = "SELECT ID FROM Users where Login='" . $inData["Login"] . "'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0)
+	{
+		// Return error because this input from the user is already associated
+		// with another account
+		$error = '{"id":0,"FirstName":"","LastName":"","error":"This account already Exists"}';
+		returnWithErrorForUser($error);
+	}
+    else {
+        $new = "INSERT INTO Users(FirstName, LastName, Login, Password) VALUES ('" . $inData["FirstName"] . "', '" . $inData["LastName"] . "', '". $inData["Login"] . "', '" . $inData["Password"] . "')";
+		$conn->query($new);
+        $newAcct = "SELECT Login, FirstName, LastName FROM Users WHERE Login='" . $inData["Login"] . "'";
+        $result = $conn->query($newAcct);
+        
+        $row = $result->fetch_assoc();
+        $firstName = $row["FirstName"];
+        $lastName = $row["LastName"];
+        $id = $row["Login"];
+        returnWithInfo($firstName, $lastName, $id);
+    }
 }
 
 function getRequestInfo()
