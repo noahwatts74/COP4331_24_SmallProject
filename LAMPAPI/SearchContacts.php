8c87a6db9322ef6ebb15b54465c7a12292d18959
HoganB <hbobertz@knights.ucf.edu>
2020-06-04 09:29:13 -0400

    search contacts implementation

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,54 @@
+<?php
+
+    $inData = getRequestInfo();
+        
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
+    $conn = new mysqli("localhost", "allanmc2_team24_1", "test1234!", "allanmc2_team24");
+    if ($conn->connect_error) 
+    {
+        returnWithError( $conn->connect_error );
+    } 
+    else
+    {   
+        $sql = "SELECT ParentLogin, FirstName, LastName, Phone, Email, Date FROM Contacts WHERE 
+                LastName LIKE '" . $inData["LastName"] . "%' AND
+                ParentLogin='" . $inData["ParentLogin"] . "'";
+        $result = mysqli_query($conn, $sql);
+        if($result->num_rows == 0){
+            returnWithError("No Contacts Found");
+        }
+        else{
+            while($row = mysqli_fetch_assoc($result)){
+                $row->error = "";
+                $test[] = $row;
+            } 
+            echo json_encode($test);
+        }
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+        return $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"ParentLogin":"0","firstName":"","lastName":"","Phone":"","Email":"","Date":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+    function returnWithInfo($rows)
+    {
+    	sendResultInfoAsJson($rows);
+    }
+
+ ?>
